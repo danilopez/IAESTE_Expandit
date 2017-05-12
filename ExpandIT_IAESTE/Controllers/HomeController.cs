7682e553951ed8f57e51efac210a989e8d6e58bd
Daniel Lopez Pedrosa <dlp@expandit.com>
2017-05-12 11:11:27 +0200

    Add sample project

---
+++ ExpandIT_IAESTE/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace ExpandIT_IAESTE.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FizzBuzz()
+        {
+            ViewBag.Message = "Write a program that prints the numbers from 1 to 100. But for multiples of three print \"Fizz\" instead of the number and for the multiples of five print \"Buzz\". For numbers which are multiples of both three and five print \"FizzBuzz\".";
+
+            return View();
+        }
+    }
+}
---
+++ ExpandIT_IAESTE/Models/Users.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExpandIT_IAESTE.Models
+{
+    public class Users
+    {
+        public string UserName { get; set; }
+        public string ContactName { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
